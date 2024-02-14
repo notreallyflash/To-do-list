27a13debff9392232b9b7f722367973c7cc7f60c
Jeffin03 <jeffyissac2203@gmail.com>
2024-02-14 12:36:03 +0530

    Added Edit functionality to Todo Details Components and some icons to the assets folder

--- src/app/todo-details/todo-details.component.ts
+++ src/app/todo-details/todo-details.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { TodoService } from '../todo.service';
+import { Location } from '@angular/common';
 import { Todo } from '../todo.model';
 
 @Component({
@@ -10,14 +12,37 @@ import { Todo } from '../todo.model';
 })
 export class TodoDetailComponent implements OnInit {
   todo: Todo | undefined;
-
+  todoForm: FormGroup = new FormGroup({});
+  editing = false;
+  
   constructor(
     private route: ActivatedRoute,
-    private todoService: TodoService
+    private todoService: TodoService,
+    private locationService: Location 
   ) {}
 
   ngOnInit(): void {
     const todoId = Number(this.route.snapshot.paramMap.get('id'));
     this.todo = this.todoService.getTodoById(todoId);
+    this.todoForm = new FormGroup({
+      title: new FormControl(null, Validators.required),
+      description: new FormControl(null, Validators.required),
+    });
+  }
+
+  onEdit() {
+    this.editing = true;
+  }
+
+  onSave() {
+    if (this.todo) {
+      const updatedTodo = { ...this.todo, ...this.todoForm.value };
+      this.todoService.updateTodo(updatedTodo);
+      this.editing = false;
+    }
+  }
+
+  goBack(){
+    this.locationService.back();
   }
 }
